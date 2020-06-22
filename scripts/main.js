ab3bcde62c62b92e909c911547701536387598fc
smonticeuex <58290699+smonticeuex@users.noreply.github.com>
2020-06-22 06:59:29 -0400

    Begin

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+//Step 1: Add Random Input
+var fuelStart = 1000000; 
+var distanceStart = 400000;
+var oxygenStart = 100;
+
+
+//Step 2: Add functions
+function newFuel (){ 
+    var elem = document.getElementById("buttons");
+	var data = ((fuelStart -10)*1.5);
+}
+
+newFuel();
+
