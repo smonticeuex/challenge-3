2a86f3e18a6bec9f32612923628d9f029c4fcf4a
smonticeuex <58290699+smonticeuex@users.noreply.github.com>
2020-06-23 06:04:10 -0400

    Done-ishh

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,42 @@
-//Step 1: Add Random Input
-var fuelStart = 1000000; 
-var distanceStart = 400000;
-var oxygenStart = 100;
+// Get the current weather conditions based on city input
+var button = document.querySelector('.button');
+var inputValue = document.querySelector('.inputValue');
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+// Display results of city input by user 
+button.addEventListener('click', function() {
+	fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=4ae86f49ba7618852f0a7820f618d869')
+	.then(response => response.json())
+	.then(data=> {
+		var nameValue = data['name'];
+		var tempValue = Math.floor(data['main']['temp']);
+		var descValue = data['weather'][0]['description'];
+		
+		
+		name.innerHTML = nameValue;
+		temp.innerHTML = tempValue; 
+		desc.innerHTML = descValue; 
+		})
+
+    .catch(err => alert("Incorrect City Name!"))
+})
+
+//Get the current time per user + AM PM format
+function earthClock () {;
+	var eClock = new Date(); 
+	
+	var hours = eClock.getHours();
+	var minutes = eClock.getMinutes(); 
+	var seconds = eClock.getSeconds();
 
 
-//Step 2: Add functions
-function newFuel (){ 
-    var elem = document.getElementById("buttons");
-	var data = ((fuelStart -10)*1.5);
+    var amPM = (hours < 12) ? "AM" : "PM";
+
+//Display the current time 
+    document.getElementById('time').innerHTML = 
+    hours + " : "+ minutes + " : " +seconds + "" + amPM;
+	var t = setTimeout(earthClock, 500); 
+
 }
-
-newFuel();
-
